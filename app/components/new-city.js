3c8b38456e884897c533c7d048c3347f854e5800
jessica fix and ryan brown <jcfix.pdx@gmail.com>
2016-03-30 08:49:57 -0700

    Add new-city component, form, and save function

---
+++ app/components/new-city.js
@@ -0,0 +1,17 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  addNewCity: false,
+  actions: {
+    cityFormShow() {
+      this.set('addNewCity', true);
+    },
+    save1() {
+      var params = {
+        city: this.get('city')
+      };
+      this.set('addNewCity', false);
+      this.sendAction('save2', params);
+    }
+  }
+});

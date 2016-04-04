9019f617f6bce0a1f0bb57fdb6b2638b68773ba4
browneryan <browneryan87@gmail.com>
2016-04-03 21:20:01 -0700

    Add sortedRentals computed property code, appears not to be functional

--- app/components/city-tile.js
+++ app/components/city-tile.js
@@ -1,9 +1,17 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  actions: {
-    destroyCity(city) {
-      this.sendAction('destroyCity', city);
+
+  fullLocation: Ember.computed('city.name', 'city.country', function() {
+      return this.get('city.name') + ', ' + this.get('city.country');
+    }),
+
+    sortBy: ['cost:asc'],
+    sortedRentals: Ember.computed.sort('city.rentals', 'sortBy'),
+
+    actions: {
+      destroyCity(city) {
+        this.sendAction('destroyCity', city);
+      }
     }
-  }
 });

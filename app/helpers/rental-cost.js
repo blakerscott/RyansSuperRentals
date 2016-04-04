fbd74498ae53dd9eab4d6e0af7c6a6fcb174c28c
Blake Scott and Ryan Brown <blakerscott@gmail.com>
2016-04-04 09:42:33 -0700

    Add parseInt in rental-cost.js

--- app/helpers/rental-cost.js
+++ app/helpers/rental-cost.js
@@ -1,16 +1,17 @@
 import Ember from 'ember';
 
 export function rentalCost(params) {
-  var rentalPrice = params[0].get('cost');
+  var price = params[0].get('cost');
+  var rentalPrice = parseInt(price);
 
   if (rentalPrice >= 150){
-    return '$$$$'
+    return '$$$$';
   } else if (rentalPrice >= 100){
-    return '$$$'
+    return '$$$';
   } else if (rentalPrice >= 50){
-    return '$$'
+    return '$$';
   } else if (rentalPrice <= 49){
-    return '$'
+    return '$';
   }
 }
 
